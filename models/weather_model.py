16499dc9ae69b4343e4b143a312b2cfa8fecbc96
Serun1017 <unive1017@gmail.com>
2026-03-07 17:13:40 +0900

    기상 데이터 보간 방식 수정 및 캐시 관리 개선(풍향 애니메이션 개선)

--- models/weather_model.py
+++ models/weather_model.py
@@ -1,4 +1,4 @@
-# weather_service.py
+# models/weather_model.py
 import pandas as pd
 import numpy as np
 from scipy.interpolate import griddata
@@ -12,6 +12,7 @@ from config import *
 # 메모리 캐시 상태 관리
 TARGET_POINTS = []
 CACHED_WIND_DATA = []
+WIND_LAST_UPDATED = 0  # 초기화 누락 변수 추가
 
 def init_target_points():
     """CSV 공간 다운샘플링 초기화"""
@@ -67,21 +68,19 @@ def fetch_weather_data():
     return obs_lons, obs_lats, obs_u, obs_v
 
 def interpolate_wind_vectors(obs_lons, obs_lats, obs_u, obs_v):
-    """Cubic 및 Nearest 이중 공간 보간"""
+    """방사형 왜곡을 방지하기 위한 선형(Linear) 공간 보간"""
     points_array = np.array(list(zip(obs_lons, obs_lats)))
     u_array, v_array = np.array(obs_u), np.array(obs_v)
     
+    # 북->남, 서->동 순서로 정규 격자 생성 (leaflet-velocity 표준)
     grid_lons = np.arange(LON_START, LON_END + GRID_RES, GRID_RES)
     grid_lats = np.arange(LAT_START, LAT_END - GRID_RES, -GRID_RES)
     grid_lon_mesh, grid_lat_mesh = np.meshgrid(grid_lons, grid_lats)
     
-    def apply_dual_interpolation(values):
-        val_cubic = griddata(points_array, values, (grid_lon_mesh, grid_lat_mesh), method='cubic')
-        val_near = griddata(points_array, values, (grid_lon_mesh, grid_lat_mesh), method='nearest')
-        return np.where(np.isnan(val_cubic), val_near, val_cubic)
-
-    grid_u = apply_dual_interpolation(u_array)
-    grid_v = apply_dual_interpolation(v_array)
+    # [핵심 수정] method='linear' 사용 및 외곽 빈 공간(바다 등)의 데이터 없는 곳은 0(무풍) 처리
+    # 기존의 cubic + nearest 조합은 보간 범위를 넘어갈 때 극단적인 값(소용돌이)을 만들어내는 원인이었음
+    grid_u = griddata(points_array, u_array, (grid_lon_mesh, grid_lat_mesh), method='linear', fill_value=0)
+    grid_v = griddata(points_array, v_array, (grid_lon_mesh, grid_lat_mesh), method='linear', fill_value=0)
     
     return np.round(grid_u.flatten(), 2).tolist(), np.round(grid_v.flatten(), 2).tolist(), len(grid_lons), len(grid_lats)
 
@@ -94,20 +93,20 @@ def format_velocity_json(u_flat, v_flat, nx_points, ny_points):
     }
     
     return [
-        {"header": {**header_base, "parameterNumber": 2}, "data": u_flat},
-        {"header": {**header_base, "parameterNumber": 3}, "data": v_flat}
+        {"header": {**header_base, "parameterNumber": 2, "parameterNumberName": "U-component_of_wind", "parameterUnit": "m.s-1"}, "data": u_flat},
+        {"header": {**header_base, "parameterNumber": 3, "parameterNumberName": "V-component_of_wind", "parameterUnit": "m.s-1"}, "data": v_flat}
     ]
 
 def update_wind_cache_job():
     """스케줄러에 등록할 전체 파이프라인 함수"""
-    global CACHED_WIND_DATA, WIND_LAST_UPDATED # WIND_LAST_UPDATED 추가
+    global CACHED_WIND_DATA, WIND_LAST_UPDATED 
     lons, lats, u, v = fetch_weather_data()
     if len(u) < 3: return
         
     u_flat, v_flat, nx_p, ny_p = interpolate_wind_vectors(lons, lats, u, v)
     CACHED_WIND_DATA = format_velocity_json(u_flat, v_flat, nx_p, ny_p)
     
-    WIND_LAST_UPDATED = time.time()  # 추가: 갱신 시간 업데이트
+    WIND_LAST_UPDATED = time.time()  
     logging.info("바람장 데이터 캐시 갱신 완료")
 
 def get_wind_data():
